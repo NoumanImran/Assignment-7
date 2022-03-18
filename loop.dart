45fc8ae34412517d183098173f80243b5e1c42bb
NoumanImran <101354576+NoumanImran@users.noreply.github.com>
2022-03-18 11:02:38 +0500

    Create loop.dart

---
+++ loop.dart
@@ -0,0 +1,65 @@
+void main(){
+
+//Program # 1
+
+  int i = 0;
+  while(i<5)
+  {
+    print("Pakistan");
+    i++;
+  }
+  
+//OUTPUT:
+// Pakistan
+// Pakistan
+// Pakistan
+// Pakistan
+// Pakistan
+
+
+//Program # 2
+
+  int a = 1;
+  while(a<=10)
+  {
+    print("$a");
+    a++;
+  }
+
+//OUTPUT:
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+
+//Program # 3
+
+ int c = 1;
+  int sum = 0;
+  while(c<=5)
+  {
+    print("$c");
+    sum = sum + c;
+    c=c+1;
+  }
+print('Sum is $sum');
+
+//OUTPUT:
+// 1
+// 2
+// 3
+// 4
+// 5
+// Sum is 15
+
+
+//Program # 4
+
+}
